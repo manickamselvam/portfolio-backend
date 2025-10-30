d1d82f27beb185ae600db8ed16e38afd2bedf2b5
Nagamanickam <nagamanickam@yahoo.co>
2025-10-30 15:41:02 +0530

    Live url of portfolio added

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const Mailgun = require("mailgun.js");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://my-new-portfolio-seven-chi.vercel.app",
+    ],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
